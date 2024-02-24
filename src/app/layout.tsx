97dd25e7043c6ad813624371ad885bc6c4c961dc
alex <simedrea.alex@gmail.com>
2024-02-24 16:17:53 +0200

    added Google Analytics again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,17 +19,15 @@ export default function RootLayout({
       <body className={`${jakarta.className} antialiased`}>
         <AppNavbar />
         {children}
-        <Script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=G-W07EGGHMEJ'
-        ></Script>
-        <Script id='google-aalytics'>
-          {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-W07EGGHMEJ');`}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-WZ7N3E1HT3"></Script>
+        <Script id='google-analytics' dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WZ7N3E1HT3');
+          `,
+        }}>
         </Script>
       </body>
     </html>
