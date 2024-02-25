631d5f99f03221b13f100f1e7c50ead04941087e
alex <simedrea.alex@gmail.com>
2024-02-25 18:48:17 +0200

    added links to locations

--- src/app/_sections/Location.tsx
+++ src/app/_sections/Location.tsx
@@ -1,4 +1,5 @@
 import { poppins } from '@/app/fonts';
+import { Link } from '@nextui-org/react';
 
 export default function Location() {
   return (
@@ -15,33 +16,59 @@ export default function Location() {
         <div className='mt-auto flex w-9/12 flex-col gap-4 max-md:w-full max-md:gap-2'>
           <div className='flex items-center rounded-md bg-accent-50 py-4'>
             <i className='fa fa-house-building mx-8 text-4xl text-accent-700 max-md:mx-4'></i>
-            <div className='mr-4'>
+            <Link
+              className='mr-4 flex flex-col items-start'
+              href='https://maps.app.goo.gl/o79oSRJGn2y1MXPs6'
+              target='_blank'
+            >
               <h4 className='text-medium font-bold'>
                 Centrul Vechi Brasov - Piata Sfatului
+                <i className='fa fa-arrow-up-right-from-square ml-2 text-lg'></i>
               </h4>
               <p className='text-medium font-medium'>20 de minute</p>
-            </div>
+            </Link>
           </div>
           <div className='flex items-center rounded-md bg-accent-50 py-4'>
             <i className='fa fa-bags-shopping mx-8 text-4xl text-accent-700 max-md:mx-4'></i>
-            <div className='mr-4'>
-              <h4 className='text-medium font-bold'>AFI Brasov</h4>
+            <Link
+              className='mr-4 flex flex-col items-start'
+              href='https://maps.app.goo.gl/TGvSTa5Hm5xXWYVaA'
+              target='_blank'
+            >
+              <h4 className='text-medium font-bold'>
+                AFI Brasov
+                <i className='fa fa-arrow-up-right-from-square ml-2 text-lg'></i>
+              </h4>
               <p className='text-medium font-medium'>25 de minute</p>
-            </div>
+            </Link>
           </div>
           <div className='flex items-center rounded-md bg-accent-50 py-4'>
             <i className='fa fa-t-rex mx-8 text-4xl text-accent-700 max-md:mx-4'></i>
-            <div className='mr-4'>
-              <h4 className='text-medium font-bold'>Dino Parc Rasnov</h4>
-              <p className='text-medium font-medium'>10 de minute</p>
-            </div>
+            <Link
+              className='mr-4 flex flex-col items-start'
+              href='https://maps.app.goo.gl/boTC3khKXmocQMHw6'
+              target='_blank'
+            >
+              <h4 className='text-medium font-bold'>
+                Dino Parc Rasnov
+                <i className='fa fa-arrow-up-right-from-square ml-2 text-lg'></i>
+              </h4>
+              <p className='text-medium font-medium'>10 minute</p>
+            </Link>
           </div>
           <div className='flex items-center rounded-md bg-accent-50 py-4'>
             <i className='fa fa-hippo mx-8 text-4xl text-accent-700 max-md:mx-4'></i>
-            <div className='mr-4'>
-              <h4 className='text-medium font-bold'>Zoo Brasov</h4>
+            <Link
+              className='mr-4 flex flex-col items-start'
+              href='https://maps.app.goo.gl/NQcpzeQ5qtmrJppj9'
+              target='_blank'
+            >
+              <h4 className='text-medium font-bold'>
+                Zoo Brasov
+                <i className='fa fa-arrow-up-right-from-square ml-2 text-lg'></i>
+              </h4>
               <p className='text-medium font-medium'>35 de minute</p>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
